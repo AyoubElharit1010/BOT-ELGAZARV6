75006c51073a267c5cb3dccf1799b6f640c33aef
AyoubElharit1010 <141142062+AyoubElharit1010@users.noreply.github.com>
2023-08-01 13:22:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,22 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
-
+global.owner = ['5321962158646077'] //ur owner number
+global.ownername = "卂ㄚㄖㄩ乃 乇ㄥ卄卂尺丨ㄒ💫" //ur owner name
+global.ytname = "ayoub_____felling" //ur yt chanel name
+global.socialm = "instagram:ayoub_____felling" //ur github or insta name
+global.location = "Africa, MOROCCO, BENI MELLAL💙" //ur location
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "5321962158646077" //ur number
+global.premium = ['5321962158646077'] //ur premium number
+global.botname = '❤️卂ㄚㄖㄩ乃 乇ㄥ卄卂尺丨ㄒ💫❤️' //ur bot name
+global.linkz = "https://youtube.com/@felling1000" //your theme url which will be displayed on whatsapp
+global.websitex = "https://linksharing.samsungcloud.com/xnevgkvew0eU" //ur website to be displayed
+global.botscript = 'https://youtube.com/@felling1000' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣ٱلَثًےـقَےـةّ⍣" //ur sticker watermark packname
+global.author = "⍣0621710767⍣" //ur sticker watermark author
+global.wm = "❤️AYOUB ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,16 +39,16 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
-    admin: '『 الامر دا لادمن بس! 』',
+    success:'تم 💚👍',
+    admin: '『❤! 』',
     botAdmin: '『 ارفع البوت ادمن الاول 』',
-    premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
-    owner: '『 الميزه دي للمطور بس! 』',
-    group: '『 الميزه دي للجروبات بس! 』',
-    private: '『 الميزه دي في الخاص بس! 』',
-    bot: '『 الميزه دي للبوت بس! 』',
-    wait: '『 استني شويه... 』',
-    linkm: '『 فين الرابط? 』',
+    premime: '『 ميزات خاصة متميزة إذا كنت ترغب في التسجيل  』',
+    owner: '『 هذه الميزة للمطور فقط! 』',
+    group: '『 هذه الميزة للمجموعات فقط! 』',
+    private: '『 هذه الميزة في الخاص فقط! 』',
+    bot: '『 هذه الميزة خاصة بالبوت! 』',
+    wait: '『 إنتظر قليلا ... 』',
+    linkm: '『 أين الرابط? 』',
     endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
 }
